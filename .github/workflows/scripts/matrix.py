546279e6ea558376042b5cf30cdbbc3c4f23b66a
jasonxue <103628493+jasonxue1@users.noreply.github.com>
2026-02-07 12:26:57 +0800

    Sync Fallen-Breath/fabric-mod-template and format *.py with ruff manually

--- .github/workflows/scripts/matrix.py
+++ .github/workflows/scripts/matrix.py
@@ -2,7 +2,8 @@
 A script to scan through the versions directory and collect all folder names as the subproject list,
 then output a json as the github action include matrix
 """
-__author__ = 'Fallen_Breath'
+
+__author__ = "Fallen_Breath"
 
 import json
 import os
@@ -10,37 +11,41 @@ import sys
 
 
 def main():
-	target_subproject_env = os.environ.get('TARGET_SUBPROJECT', '')
-	target_subprojects = list(filter(None, target_subproject_env.split(',') if target_subproject_env != '' else []))
-	print('target_subprojects: {}'.format(target_subprojects))
+    target_subproject_env = os.environ.get("TARGET_SUBPROJECT", "")
+    target_subprojects = [x for x in target_subproject_env.split(",") if x]
+    print("target_subprojects: {}".format(target_subprojects))
 
-	with open('settings.json') as f:
-		settings: dict = json.load(f)
+    with open("settings.json") as f:
+        settings: dict = json.load(f)
 
-	if len(target_subprojects) == 0:
-		subprojects = settings['versions']
-	else:
-		subprojects = []
-		for subproject in settings['versions']:
-			if subproject in target_subprojects:
-				subprojects.append(subproject)
-				target_subprojects.remove(subproject)
-		if len(target_subprojects) > 0:
-			print('Unexpected subprojects: {}'.format(target_subprojects), file=sys.stderr)
-			sys.exit(1)
+    if len(target_subprojects) == 0:
+        subprojects = settings["versions"]
+    else:
+        subprojects = []
+        for subproject in settings["versions"]:
+            if subproject in target_subprojects:
+                subprojects.append(subproject)
+                target_subprojects.remove(subproject)
+        if len(target_subprojects) > 0:
+            print(
+                "Unexpected subprojects: {}".format(target_subprojects), file=sys.stderr
+            )
+            sys.exit(1)
 
-	matrix_entries = []
-	for subproject in subprojects:
-		matrix_entries.append({
-			'subproject': subproject,
-		})
-	matrix = {'include': matrix_entries}
-	with open(os.environ['GITHUB_OUTPUT'], 'w') as f:
-		f.write('matrix={}\n'.format(json.dumps(matrix)))
+    matrix_entries = []
+    for subproject in subprojects:
+        matrix_entries.append(
+            {
+                "subproject": subproject,
+            }
+        )
+    matrix = {"include": matrix_entries}
+    with open(os.environ["GITHUB_OUTPUT"], "w") as f:
+        f.write("matrix={}\n".format(json.dumps(matrix)))
 
-	print('matrix:')
-	print(json.dumps(matrix, indent=2))
+    print("matrix:")
+    print(json.dumps(matrix, indent=2))
 
 
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
